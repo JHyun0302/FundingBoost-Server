674516ecd5d5edc02a83ea4228ebcc30dfdff67a
JHyun0302 <wogustkfkd11@gmail.com>
2024-04-09 16:31:23 +0900

    :tada: [Init] Initial Commit

---
+++ src/main/java/kcs/funding/fundingboost/FundingBoostApplication.java
@@ -0,0 +1,13 @@
+package kcs.funding.fundingboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundingBoostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundingBoostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kcs/funding/fundingboost/FundingBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package kcs.funding.fundingboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundingBoostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
