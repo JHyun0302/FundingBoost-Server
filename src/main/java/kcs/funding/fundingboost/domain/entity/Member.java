f0a6b73c50f32e8fbc83f70df022a7beb26f5392
JHyun0302 <wogustkfkd11@gmail.com>
2024-04-11 16:31:17 +0900

    :sparkles: member entity 추가

---
+++ src/main/java/kcs/funding/fundingboost/domain/entity/Member.java
@@ -0,0 +1,45 @@
+package kcs.funding.fundingboost.domain.entity;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(
+        name = "member"
+)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @NotNull
+    @Column(name = "nick_name", length = 20)
+    private String nickName;
+
+    @NotNull
+    @Column(length = 50)
+    private String email;
+
+    @NotNull
+    @Column(name = "profile_img_url", length = 100)
+    private String profileImgUrl;
+
+    @ColumnDefault("0")
+    @NotNull
+    private int point;
+
+}
