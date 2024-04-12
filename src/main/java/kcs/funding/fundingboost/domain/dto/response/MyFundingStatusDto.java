5e9fd0c43d9c19ee48b3218c9b4189a30715485e
koosco <rnxogud123@naver.com>
2024-04-12 15:51:11 +0900

    feat: home page MyFundingStatusDto 추가
    * fundingId: Long
    * fundingPercent: int
    * deadline: String

--- src/main/java/kcs/funding/fundingboost/domain/dto/response/MyFundingStatusDto.java
+++ src/main/java/kcs/funding/fundingboost/domain/dto/response/MyFundingStatusDto.java
@@ -7,14 +7,10 @@ import lombok.Builder;
 
 @Builder
 public record MyFundingStatusDto(Long fundingId,
-                                 float fundingPercent,
+                                 int fundingPercent,
                                  String deadline) {
 
-    public static MyFundingStatusDto fromEntity(Funding funding) {
-        float currentPercent = (float) funding.getCollectPrice() / funding.getTotalPrice();
-        int leftDate = (int) ChronoUnit.DAYS.between(LocalDate.now(), funding.getDeadline());
-        String deadline = "D-" + leftDate;
-
+    public static MyFundingStatusDto fromEntity(Funding funding, int currentPercent, String deadline) {
         return MyFundingStatusDto.builder()
             .fundingId(funding.getFundingId())
             .fundingPercent(currentPercent)
