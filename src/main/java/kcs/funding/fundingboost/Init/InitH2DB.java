6f8bef0aadfe2846f623924c8eab0c10e778bd3c
koosco <rnxogud123@naver.com>
2024-04-16 13:54:37 +0900

    feat: 친구 펀딩하기 더미 데이터 추가

--- src/main/java/kcs/funding/fundingboost/Init/InitH2DB.java
+++ src/main/java/kcs/funding/fundingboost/Init/InitH2DB.java
@@ -2,11 +2,17 @@ package kcs.funding.fundingboost.Init;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
-
 import java.time.LocalDateTime;
 import java.util.List;
-
-import kcs.funding.fundingboost.domain.entity.*;
+import kcs.funding.fundingboost.domain.entity.Delivery;
+import kcs.funding.fundingboost.domain.entity.Funding;
+import kcs.funding.fundingboost.domain.entity.FundingItem;
+import kcs.funding.fundingboost.domain.entity.GiftHubItem;
+import kcs.funding.fundingboost.domain.entity.Item;
+import kcs.funding.fundingboost.domain.entity.Member;
+import kcs.funding.fundingboost.domain.entity.Order;
+import kcs.funding.fundingboost.domain.entity.Relationship;
+import kcs.funding.fundingboost.domain.entity.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -16,41 +22,43 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @RequiredArgsConstructor
 public class InitH2DB {
+
     private final InitService initService;
 
     @PostConstruct
     public void init() {
 //        initService.init();
 //        initService.mainPageTestInit();
-        initService.friendFundingPayView();
+//        initService.friendFundingPayView();
+        initService.friendFunding();
     }
 
     @Component
     @Transactional
     @RequiredArgsConstructor
     @Slf4j
-    static class InitService{
+    static class InitService {
 
         private final EntityManager em;
 
         public void init() {
             Member member1 = Member.createMember("nickname1", "email@gmail.com", "url");
-            Member member2 = Member.createMemberWithPoint("nickname2", "email2@gmail.com", "url", 20000);
+            Member member2 = Member.createMemberWithPoint("nickname2", "email2@gmail.com", "url",
+                20000);
             em.persist(member1);
             em.persist(member2);
 
             Item item1 = Item.createItem("그릭요거트 딸기 생크림 피스 + 아메리카노 (R)", 10700,
-                    "https://gift.kakao.com/product/5543372", "투썸플레이스",
-                    "카페", "");
+                "https://gift.kakao.com/product/5543372", "투썸플레이스",
+                "카페", "");
             Item item2 = Item.createItem("[예스24] 양수인간 : 삶의 격을 높이는 내면 변화 심리학", 17820
-                    , "https://gift.kakao.com/product/9443715", "인문",
-                    "책", "");
+                , "https://gift.kakao.com/product/9443715", "인문",
+                "책", "");
 
             Item item3 = Item.createItem(
-                    "[각인/선물포장] NEW 디올 어딕트 립스틱", 57000,
-                    "https://gift.kakao.com/product/4418907", "디올",
-                    "화장", "[NEW] 481 데지");
-
+                "[각인/선물포장] NEW 디올 어딕트 립스틱", 57000,
+                "https://gift.kakao.com/product/4418907", "디올",
+                "화장", "[NEW] 481 데지");
 
             em.persist(item1);
             em.persist(item2);
@@ -61,16 +69,18 @@ public class InitH2DB {
             em.persist(order1);
             em.persist(order2);
 
-            Delivery delivery1  = Delivery.createDelivery("서울시 가산 디지털단지", "010-1111-1111", "nickname1", member1, item1);
-            Delivery delivery2  = Delivery.createDelivery("경기도 오산시", "010-2222-2222", "nickname2", member1, item2);
+            Delivery delivery1 = Delivery.createDelivery("서울시 가산 디지털단지", "010-1111-1111",
+                "nickname1", member1, item1);
+            Delivery delivery2 = Delivery.createDelivery("경기도 오산시", "010-2222-2222", "nickname2",
+                member1, item2);
             em.persist(delivery1);
             em.persist(delivery2);
 
-
-            Funding funding = Funding.createFundingWithCollectPrice(member1, "Thanks", Tag.BIRTHDAY, 57000, 12000, LocalDateTime.of(2023,12,27,0,0,0));
+            Funding funding = Funding.createFundingWithCollectPrice(member1, "Thanks", Tag.BIRTHDAY,
+                57000, 12000, LocalDateTime.of(2023, 12, 27, 0, 0, 0));
             em.persist(funding);
 
-            FundingItem fundingItem = FundingItem.createFundingItem(funding,item3,1);
+            FundingItem fundingItem = FundingItem.createFundingItem(funding, item3, 1);
             em.persist(fundingItem);
 
             GiftHubItem giftHubItem1 = GiftHubItem.createGiftHubItem(1, item1, member1);
@@ -130,9 +140,10 @@ public class InitH2DB {
                 1);
             em.persist(friendFundingItem1);
         }
-        
+
         public void friendFundingPayView() {
-            Member member1 = Member.createMemberWithPoint("nickname", "email@gmail.com", "url", 100000);
+            Member member1 = Member.createMemberWithPoint("nickname", "email@gmail.com", "url",
+                100000);
             Member member2 = Member.createMember("friend1", "email2@gmail.com", "url2");
             Member member3 = Member.createMember("friend2", "email3@gmail.com", "url3");
             em.persist(member1);
@@ -140,6 +151,18 @@ public class InitH2DB {
             em.persist(member3);
 
             // 친구 펀딩 추가
+            Funding friendFunding = Funding.createFunding(member2, "생일축하해줘", Tag.BIRTHDAY, 12341234,
+                LocalDateTime.now().plusDays(14));
+            em.persist(friendFunding);
+        }
+
+        public void friendFunding() {
+            Member member1 = Member.createMemberWithPoint("nickname", "email@gmail.com", "url",
+                100000);
+            Member member2 = Member.createMember("friend1", "email2@gmail.com", "url2");
+            em.persist(member1);
+            em.persist(member2);
+
             Funding friendFunding = Funding.createFunding(member2, "생일축하해줘", Tag.BIRTHDAY, 12341234,
                 LocalDateTime.now().plusDays(14));
             em.persist(friendFunding);
