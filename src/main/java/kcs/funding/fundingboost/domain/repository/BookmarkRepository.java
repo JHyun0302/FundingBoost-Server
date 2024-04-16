37ac69a69790f16071c71f1afc70d6241ca5e376
JHyun0302 <wogustkfkd11@gmail.com>
2024-04-16 11:37:18 +0900

    :sparkles: [Feat] 쇼핑 상세 페이지 조회

--- src/main/java/kcs/funding/fundingboost/domain/repository/BookmarkRepository.java
+++ src/main/java/kcs/funding/fundingboost/domain/repository/BookmarkRepository.java
@@ -1,7 +1,12 @@
 package kcs.funding.fundingboost.domain.repository;
 
+import java.util.Optional;
 import kcs.funding.fundingboost.domain.entity.Bookmark;
+import kcs.funding.fundingboost.domain.entity.Item;
+import kcs.funding.fundingboost.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    Optional<Bookmark> findBookmarkByMemberAndItem(Member member, Item item);
 }
--- src/main/java/kcs/funding/fundingboost/domain/service/ItemService.java
+++ src/main/java/kcs/funding/fundingboost/domain/service/ItemService.java
@@ -1,10 +1,16 @@
 package kcs.funding.fundingboost.domain.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import kcs.funding.fundingboost.domain.dto.response.ItemDetailDto;
 import kcs.funding.fundingboost.domain.dto.response.ShopDto;
+import kcs.funding.fundingboost.domain.entity.Bookmark;
 import kcs.funding.fundingboost.domain.entity.Item;
+import kcs.funding.fundingboost.domain.entity.Member;
+import kcs.funding.fundingboost.domain.repository.BookmarkRepository;
 import kcs.funding.fundingboost.domain.repository.ItemRepository;
+import kcs.funding.fundingboost.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,11 +23,30 @@ import org.springframework.transaction.annotation.Transactional;
 public class ItemService {
     private final ItemRepository itemRepository;
 
+    private final MemberRepository memberRepository;
+
+    private final BookmarkRepository bookmarkRepository;
+
     public List<ShopDto> getItems() {
         List<Item> items = itemRepository.findAll();
 
         return items.stream()
                 .map(ShopDto::createGiftHubDto)
                 .collect(Collectors.toList());
+    }
+
+    public ItemDetailDto getItemDetail(Long memberId, Long itemId) {
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new RuntimeException("Item Not Found"));
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("Member not found"));
+
+        Optional<Bookmark> bookmark = bookmarkRepository.findBookmarkByMemberAndItem(member, item);
+        if (bookmark.isPresent()) {
+            return ItemDetailDto.fromEntity(item, true);
+        } else {
+            return ItemDetailDto.fromEntity(item, false);
+        }
     }
 }
