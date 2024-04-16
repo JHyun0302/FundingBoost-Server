e6e2f9294b6a81b06c30e296b136c2d92d3c7ab8
JHyun0302 <wogustkfkd11@gmail.com>
2024-04-16 16:01:15 +0900

    :recycle: [Refactor] 사용하지 않는 requestdto 메서드 삭제

--- src/main/java/kcs/funding/fundingboost/domain/dto/request/RegisterFundingItemDto.java
+++ src/main/java/kcs/funding/fundingboost/domain/dto/request/RegisterFundingItemDto.java
@@ -1,10 +1,4 @@
 package kcs.funding.fundingboost.domain.dto.request;
 
-public record RegisterFundingItemDto(
-        Long itemId,
-        int itemSequence
-) {
-    public static RegisterFundingItemDto createRegisterFundingItemDto(Long itemId, int itemSequence) {
-        return new RegisterFundingItemDto(itemId, itemSequence);
-    }
+public record RegisterFundingItemDto(Long itemId, int itemSequence) {
 }
