ebc68165d7c157c9caea2844d086621c86733293
koosco <rnxogud123@naver.com>
2024-04-16 11:37:09 +0900

    feat: friendFundingPay 페이지 조회

---
+++ src/main/java/kcs/funding/fundingboost/domain/controller/FriendPayController.java
@@ -0,0 +1,24 @@
+package kcs.funding.fundingboost.domain.controller;
+
+import kcs.funding.fundingboost.domain.dto.response.FriendFundingPayingDto;
+import kcs.funding.fundingboost.domain.service.FriendPayService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/friends/pay")
+public class FriendPayController {
+
+    private final FriendPayService friendPayService;
+
+    @GetMapping("/{fundingId}")
+    public FriendFundingPayingDto friendPayView(@RequestParam("memberId") Long memberId,
+        @PathVariable("fundingId") Long fundingId) {
+        return friendPayService.findFriendFundingPay(fundingId, memberId);
+    }
+}
--- src/main/java/kcs/funding/fundingboost/domain/dto/response/DeliveryDto.java
+++ src/main/java/kcs/funding/fundingboost/domain/dto/response/DeliveryDto.java
@@ -13,5 +13,4 @@ public record DeliveryDto(String customerName,
                 delivery.getAddress(),
                 delivery.getPhoneNumber());
     }
-    //
 }
