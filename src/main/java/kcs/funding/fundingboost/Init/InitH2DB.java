c218e0ab3c3f0afba5c32c5263ce6d8a0698c564
koosco <rnxogud123@naver.com>
2024-04-17 17:53:28 +0900

    fix: orderStatus 및 deliveryStatus 삭제

--- src/main/java/kcs/funding/fundingboost/Init/InitH2DB.java
+++ src/main/java/kcs/funding/fundingboost/Init/InitH2DB.java
@@ -87,8 +87,8 @@ public class InitH2DB {
             Delivery delivery1 = deliveries.get(0);
             Delivery delivery2 = deliveries.get(1);
 
-            Order order1 = Order.createOrder(1, 10700, false, item1, member1, delivery1);
-            Order order2 = Order.createOrder(1, 17820, false, item2, member2, delivery2);
+            Order order1 = Order.createOrder(1, 10700, item1, member1, delivery1);
+            Order order2 = Order.createOrder(1, 17820, item2, member2, delivery2);
             em.persist(order1);
             em.persist(order2);
         }
