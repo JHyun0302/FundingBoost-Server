8295cd4cfe013c219b8ce0720b057b57c8079ae6
koosco <rnxogud123@naver.com>
2024-04-25 13:33:35 +0900

    feat: OrderItem 엔티티 추가
    * Order, Item과 다대일 관계

---
+++ src/main/java/kcs/funding/fundingboost/domain/entity/OrderItem.java
@@ -0,0 +1,37 @@
+package kcs.funding.fundingboost.domain.entity;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "order_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Order order;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "item_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Item item;
+}
