1b51a0a7265a0382bf764363a8287d00cb093c95
koosco <rnxogud123@naver.com>
2024-04-25 19:52:03 +0900

    refactor: LocalDateTime으로 가져오도록 수정

--- src/main/java/kcs/funding/fundingboost/domain/dto/response/myPage/orderHistory/OrderHistoryItemDto.java
+++ src/main/java/kcs/funding/fundingboost/domain/dto/response/myPage/orderHistory/OrderHistoryItemDto.java
@@ -1,6 +1,6 @@
 package kcs.funding.fundingboost.domain.dto.response.myPage.orderHistory;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import kcs.funding.fundingboost.domain.entity.OrderItem;
 import lombok.Builder;
 
@@ -11,7 +11,7 @@ public record OrderHistoryItemDto(
         String optionName,
         int quantity,
         int price,
-        LocalDate createdDate
+        LocalDateTime createdDate
 ) {
 
     public static OrderHistoryItemDto fromEntity(OrderItem orderItem) {
@@ -21,7 +21,7 @@ public record OrderHistoryItemDto(
                 .optionName(orderItem.getItem().getOptionName())
                 .quantity(orderItem.getQuantity())
                 .price(orderItem.getItem().getItemPrice() * orderItem.getQuantity()) // 총 가격을 반환
-                .createdDate(LocalDate.from(orderItem.getOrder().getCreatedDate()))
+                .createdDate(orderItem.getOrder().getCreatedDate())
                 .build();
     }
 }
