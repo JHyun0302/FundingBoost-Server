ebfb26d57f585e6acf72600e745876b28b56bc88
ChangHee98 <70644043+ChangHee98@users.noreply.github.com>
2024-04-27 17:13:55 +0900

    feat: Gifthub 상품 삭제 기능 구현

--- src/main/java/kcs/funding/fundingboost/domain/service/GiftHubItemService.java
+++ src/main/java/kcs/funding/fundingboost/domain/service/GiftHubItemService.java
@@ -1,11 +1,12 @@
 package kcs.funding.fundingboost.domain.service;
 
 import static kcs.funding.fundingboost.domain.dto.response.GiftHubDto.createGiftHubDto;
+import static kcs.funding.fundingboost.domain.exception.ErrorCode.NOT_FOUND_GIFTHUB_ITEM;
 import static kcs.funding.fundingboost.domain.exception.ErrorCode.NOT_FOUND_ITEM;
 import static kcs.funding.fundingboost.domain.exception.ErrorCode.NOT_FOUND_MEMBER;
-import static kcs.funding.fundingboost.domain.exception.ErrorCode.NOT_FOUND_GIFTHUB_ITEM;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import kcs.funding.fundingboost.domain.dto.common.CommonSuccessDto;
 import kcs.funding.fundingboost.domain.dto.request.AddGiftHubDto;
@@ -65,6 +66,16 @@ public class GiftHubItemService {
         GiftHubItem giftHubItem = giftHubItemRepository.findById(gifthubItemId)
                 .orElseThrow(() -> new CommonException(NOT_FOUND_GIFTHUB_ITEM));
         giftHubItem.updateQuantity(itemQuantity.quantity());
+        return CommonSuccessDto.fromEntity(true);
+    }
+
+    @Transactional
+    public CommonSuccessDto deleteGiftHubItem(Long memberId, Long gifthubItemId) {
+        Optional<GiftHubItem> gifthubItem = giftHubItemRepository.findById(gifthubItemId);
+        if (gifthubItem.isEmpty()) {
+            throw new CommonException(NOT_FOUND_GIFTHUB_ITEM);
+        }
+        giftHubItemRepository.deleteById(gifthubItemId);
         return CommonSuccessDto.fromEntity(true);
     }
 }
