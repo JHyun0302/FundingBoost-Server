559a6e79bef08ed7a3aab2242d1cdcc6b0dabffd
MeangSung <aoddlsgh98@gmail.com>
2024-04-29 11:53:54 +0900

    refactor 변수명 수정

--- src/main/java/kcs/funding/fundingboost/domain/dto/response/myPage/friendFundingHistory/FriendFundingContributionDto.java
+++ src/main/java/kcs/funding/fundingboost/domain/dto/response/myPage/friendFundingHistory/FriendFundingContributionDto.java
@@ -9,14 +9,14 @@ import lombok.Builder;
 @Builder
 public record FriendFundingContributionDto(String nickname,
                                            int price,
-                                           String itemImageUrl,
+                                           String friendProfileImg,
                                            Tag tag,
                                            LocalDateTime createdDate) {
     public static FriendFundingContributionDto fromEntity(Contributor contributor, Funding friendFunding) {
         return FriendFundingContributionDto.builder()
                 .nickname(friendFunding.getMember().getNickName())
                 .price(contributor.getContributorPrice())
-                .itemImageUrl(friendFunding.getMember().getProfileImgUrl())
+                .friendProfileImg(friendFunding.getMember().getProfileImgUrl())
                 .tag(friendFunding.getTag())
                 .createdDate(friendFunding.getCreatedDate())
                 .build();
