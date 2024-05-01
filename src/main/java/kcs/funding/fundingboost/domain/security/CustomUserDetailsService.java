d340d1c91fe7b805b59d527f290330484a03eb80
koosco <rnxogud123@naver.com>
2024-05-01 11:34:53 +0900

    feat: CustomUserDetails, Service 추가

---
+++ src/main/java/kcs/funding/fundingboost/domain/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package kcs.funding.fundingboost.domain.security;
+
+import kcs.funding.fundingboost.domain.entity.member.Member;
+import kcs.funding.fundingboost.domain.exception.CommonException;
+import kcs.funding.fundingboost.domain.exception.ErrorCode;
+import kcs.funding.fundingboost.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByNickName(username).orElseThrow(
+                () -> new CommonException(ErrorCode.NOT_FOUND_MEMBER)
+        );
+
+        return new CustomUserDetails(member);
+    }
+}
