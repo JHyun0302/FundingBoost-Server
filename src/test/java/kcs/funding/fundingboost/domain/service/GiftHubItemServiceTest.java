168ad73f491ccfac29dc45bbdd11fac8f374591d
JHyun0302 <wogustkfkd11@gmail.com>
2024-05-01 14:39:01 +0900

    :white_check_mark: [Test] GiftHubItemService 테스트 코드 작성

--- src/test/java/kcs/funding/fundingboost/domain/service/GiftHubItemServiceTest.java
+++ src/test/java/kcs/funding/fundingboost/domain/service/GiftHubItemServiceTest.java
@@ -9,7 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
@@ -32,8 +34,6 @@ import kcs.funding.fundingboost.domain.repository.giftHubItem.GiftHubItemReposit
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -54,27 +54,24 @@ class GiftHubItemServiceTest {
     private GiftHubItemService giftHubItemService;
 
     private Member member;
+    private Item item1;
+    private Item item2;
+    private GiftHubItem giftHubItem1;
+    private GiftHubItem giftHubItem2;
 
     @BeforeEach
     void setUp() {
         member = createMember();
+        item1 = createItem1();
+        item2 = createItem2();
+        giftHubItem1 = GiftHubItem.createGiftHubItem(1, item1, member);
+        giftHubItem2 = GiftHubItem.createGiftHubItem(2, item2, member);
     }
 
     @DisplayName("로그인 한 사용자의 기프트 허브 정보")
-    @ParameterizedTest(name = "{index} {displayName} arguments = {arguments}")
-    @CsvSource({
-            "NEW 루쥬 알뤼르 벨벳 뉘 블랑쉬 리미티드 에디션, 61000, https://img1.kakaocdn.net/..., 샤넬, 뷰티, 00:00, NEW 루쥬 코코 밤(+샤넬 기프트 카드), 51000, https://img1.kakaocdn.net/..., 샤넬, 뷰티, 934 코랄린 [NEW]"
-    })
-    void getGiftHub_ReturnsGiftHubDtoList_WhenMemberExists(String itemName1, int itemPrice1, String itemImageUrl1,
-                                                           String brandName1, String category1, String optionName1,
-                                                           String itemName2, int itemPrice2, String itemImageUrl2,
-                                                           String brandName2, String category2, String optionName2) {
+    @Test
+    void getGiftHub_ReturnsGiftHubDtoList_WhenMemberExists() {
         //given
-        Item item1 = createItem(itemName1, itemPrice1, itemImageUrl1, brandName1, category1, optionName1);
-        Item item2 = createItem(itemName2, itemPrice2, itemImageUrl2, brandName2, category2, optionName2);
-        GiftHubItem giftHubItem1 = GiftHubItem.createGiftHubItem(1, item1, member);
-        GiftHubItem giftHubItem2 = GiftHubItem.createGiftHubItem(2, item2, member);
-
         when(memberRepository.findById(member.getMemberId())).thenReturn(Optional.of(member));
         when(giftHubItemRepository.findGiftHubItemsByMember(member.getMemberId())).thenReturn(
                 List.of(giftHubItem1, giftHubItem2));
@@ -105,22 +102,13 @@ class GiftHubItemServiceTest {
     }
 
     @DisplayName("GiftHubItem 추가 성공")
-    @ParameterizedTest(name = "{index} {displayName} arguments = {arguments}")
-    @CsvSource({
-            "NEW 루쥬 알뤼르 벨벳 뉘 블랑쉬 리미티드 에디션, 61000, https://img1.kakaocdn.net/..., 샤넬, 뷰티, 00:00, NEW 루쥬 코코 밤(+샤넬 기프트 카드), 51000, https://img1.kakaocdn.net/..., 샤넬, 뷰티, 934 코랄린 [NEW]"
-    })
-    void addGiftHub(String itemName1, int itemPrice1, String itemImageUrl1,
-                    String brandName1, String category1, String optionName1,
-                    String itemName2, int itemPrice2, String itemImageUrl2,
-                    String brandName2, String category2, String optionName2)
-            throws NoSuchFieldException, IllegalAccessException {
-
+    @Test
+    void addGiftHub() throws NoSuchFieldException, IllegalAccessException {
         //given
-        Item item1 = createItem(itemName1, itemPrice1, itemImageUrl1, brandName1, category1, optionName1);
         Field itemId1 = item1.getClass().getDeclaredField("itemId");
         itemId1.setAccessible(true);
         itemId1.set(item1, 1L);
-        Item item2 = createItem(itemName2, itemPrice2, itemImageUrl2, brandName2, category2, optionName2);
+
         Field itemId2 = item1.getClass().getDeclaredField("itemId");
         itemId2.setAccessible(true);
         itemId2.set(item2, 2L);
@@ -161,24 +149,20 @@ class GiftHubItemServiceTest {
     }
 
     @DisplayName("GiftHubItem 추가 실패 - 멤버를 찾을 수 없음")
-    @ParameterizedTest(name = "{index} {displayName} arguments = {arguments}")
-    @CsvSource({"NEW 루쥬 알뤼르 벨벳 뉘 블랑쉬 리미티드 에디션, 61000, https://img1.kakaocdn.net/..., 샤넬, 뷰티, 00:00"})
-    void addGiftHub_Fail_MemberNotFound(String itemName, int itemPrice, String itemImageUrl,
-                                        String brandName, String category, String optionName)
-            throws NoSuchFieldException, IllegalAccessException {
+    @Test
+    void addGiftHub_Fail_MemberNotFound() throws NoSuchFieldException, IllegalAccessException {
         // given
-        Item item = createItem(itemName, itemPrice, itemImageUrl, brandName, category, optionName);
         //리플렉션을 이용한 itemId 강제 주입
-        Field itemId = item.getClass().getDeclaredField("itemId");
+        Field itemId = item1.getClass().getDeclaredField("itemId");
         itemId.setAccessible(true);
-        itemId.set(item, 1L);
-        when(itemRepository.findById(item.getItemId())).thenReturn(Optional.of(item));
+        itemId.set(item1, 1L);
+        when(itemRepository.findById(item1.getItemId())).thenReturn(Optional.of(item1));
         when(memberRepository.findById(member.getMemberId())).thenReturn(Optional.empty());
         AddGiftHubDto addGiftHubDto = new AddGiftHubDto(member.getMemberId(), 1);
 
         // when
         Exception exception = assertThrows(CommonException.class, () -> {
-            giftHubItemService.addGiftHub(item.getItemId(), addGiftHubDto);
+            giftHubItemService.addGiftHub(item1.getItemId(), addGiftHubDto);
         });
 
         // then
@@ -190,17 +174,16 @@ class GiftHubItemServiceTest {
     @Test
     void updateItem_Success() {
         //given
-        Long gifthubItemId = 1L;
         ItemQuantityDto itemQuantityDto = new ItemQuantityDto(10);
         GiftHubItem mockGiftHubItem = mock(GiftHubItem.class);
 
-        when(giftHubItemRepository.findById(gifthubItemId)).thenReturn(Optional.of(mockGiftHubItem));
+        when(giftHubItemRepository.findById(giftHubItem1.getGiftHunItemId())).thenReturn(Optional.of(mockGiftHubItem));
 
         //when
-        CommonSuccessDto result = giftHubItemService.updateItem(gifthubItemId, itemQuantityDto);
+        CommonSuccessDto result = giftHubItemService.updateItem(giftHubItem1.getGiftHunItemId(), itemQuantityDto);
 
         //then
-        verify(giftHubItemRepository).findById(gifthubItemId);
+        verify(giftHubItemRepository).findById(giftHubItem1.getGiftHunItemId());
         verify(mockGiftHubItem).updateQuantity(itemQuantityDto.quantity());
         assertTrue(result.isSuccess());
     }
@@ -225,13 +208,33 @@ class GiftHubItemServiceTest {
     @DisplayName("GiftHubItem 삭제 성공")
     @Test
     void deleteGiftHubItem_Success() {
+        //given
+        when(giftHubItemRepository.findGiftHubItemByGiftHubItemIdAndMemberId(giftHubItem1.getGiftHunItemId(),
+                member.getMemberId())).thenReturn(Optional.of(giftHubItem1));
 
+        //when
+        CommonSuccessDto result = giftHubItemService.deleteGiftHubItem(member.getMemberId(),
+                giftHubItem1.getGiftHunItemId());
+
+        //then
+        assertTrue(result.isSuccess());
+        verify(giftHubItemRepository).deleteById(giftHubItem1.getGiftHunItemId());
     }
 
     @DisplayName("GiftHubItem 삭제 실패 - 아이템 조회 실패")
     @Test
     void deleteGiftHubItem_ItemNotFound() {
+        //given
+        when(giftHubItemRepository.findGiftHubItemByGiftHubItemIdAndMemberId(giftHubItem1.getGiftHunItemId(),
+                member.getMemberId())).thenReturn(Optional.empty());
 
+        //when
+        CommonException exception = assertThrows(CommonException.class,
+                () -> giftHubItemService.deleteGiftHubItem(member.getMemberId(), giftHubItem1.getGiftHunItemId()));
+
+        //then
+        assertEquals(NOT_FOUND_GIFTHUB_ITEM.getMessage(), exception.getMessage());
+        verify(giftHubItemRepository, never()).deleteById(anyLong());
     }
 
 
@@ -242,8 +245,15 @@ class GiftHubItemServiceTest {
                 "", "aFxoWGFUZlV5SH9MfE9-TH1PY1JiV2JRaF83");
     }
 
-    private static Item createItem(String itemName, int itemPrice, String itemImageUrl, String brandName,
-                                   String category, String optionName) {
-        return Item.createItem(itemName, itemPrice, itemImageUrl, brandName, category, optionName);
+    private static Item createItem1() {
+        return Item.createItem("NEW 루쥬 알뤼르 벨벳 뉘 블랑쉬 리미티드 에디션", 61000,
+                "https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20240319133310_1fda0cf74e4f43608184bce3050ae22a.jpg",
+                "샤넬", "뷰티", "00:00");
+    }
+
+    private static Item createItem2() {
+        return Item.createItem("NEW 루쥬 코코 밤(+샤넬 기프트 카드)", 51000,
+                "https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20220111185052_b92447cb764d470ead70b2d0fe75fe5c.jpg",
+                "샤넬", "뷰티", "934 코랄린 [NEW]");
     }
 }
