b17d040ec52ea6fd7bbfa81237cdcd70ce42d6c8
MeangSung <aoddlsgh98@gmail.com>
2024-05-01 09:43:32 +0900

    test: bookmark 페이지 조회 테스트 코드

--- src/test/java/kcs/funding/fundingboost/domain/service/BookmarkServiceTest.java
+++ src/test/java/kcs/funding/fundingboost/domain/service/BookmarkServiceTest.java
@@ -1,13 +1,18 @@
 package kcs.funding.fundingboost.domain.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 import kcs.funding.fundingboost.domain.dto.common.CommonSuccessDto;
+import kcs.funding.fundingboost.domain.dto.response.myPage.wishList.MyBookmarkListDto;
 import kcs.funding.fundingboost.domain.entity.Bookmark;
 import kcs.funding.fundingboost.domain.entity.Item;
 import kcs.funding.fundingboost.domain.entity.Member;
@@ -22,6 +27,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @SpringBootTest
 class BookmarkServiceTest {
@@ -40,13 +46,61 @@ class BookmarkServiceTest {
 
     private Member member;
 
+    private Bookmark bookmark1;
+
+    private Bookmark bookmark2;
+
+    private Item item1;
+
+    private Item item2;
+
     @BeforeEach
     void setUp() {
         member = createMember();
+        item1 = createItem1();
+        item2 = createItem2();
     }
 
+    @DisplayName("북마크 존재 시: 북마크 목록 조회 성공")
     @Test
-    void getMyWishList() {
+    public void getMyWishList_Success() {
+        //given
+        ReflectionTestUtils.setField(member, "memberId", 1L);
+        ReflectionTestUtils.setField(item1, "itemId", 1L);
+        ReflectionTestUtils.setField(item2, "itemId", 2L);
+
+        Bookmark bookmark1 = Bookmark.createBookmark(member, item1);
+        Bookmark bookmark2 = Bookmark.createBookmark(member, item2);
+
+        when(memberRepository.findById(anyLong())).thenReturn(Optional.of(member));
+
+        when(bookmarkRepository.findAllByMemberId(member.getMemberId())).thenReturn(List.of(bookmark1, bookmark2));
+
+        //when
+        MyBookmarkListDto resultDto = bookmarkService.getMyWishList(member.getMemberId());
+
+        //then
+        assertNotNull(resultDto);
+        assertThat(resultDto.myPageMemberDto().nickname()).isEqualTo(member.getNickName());
+        assertThat(resultDto.bookmarkItemDtos()).hasSize(2).extracting("itemId")
+                .contains(item1.getItemId(), item2.getItemId());
+    }
+
+    @DisplayName("북마크가 존재하지 않을 시: 북마크 빈 리스트 값으로 반환")
+    @Test
+    public void getMyWishList_Null() {
+        //given
+        ReflectionTestUtils.setField(member, "memberId", 1L);
+
+        when(memberRepository.findById(member.getMemberId())).thenReturn(Optional.of(member));
+
+        //when
+        MyBookmarkListDto resultDto = bookmarkService.getMyWishList(member.getMemberId());
+
+        //then
+        assertNotNull(resultDto);
+        assertThat(resultDto.myPageMemberDto().nickname()).isEqualTo(member.getNickName());
+        assertThat(resultDto.bookmarkItemDtos()).isEmpty();
     }
 
     @DisplayName("북마크가 존재할 때 : 북마크 삭제")
@@ -109,4 +163,18 @@ class BookmarkServiceTest {
                                    String category, String optionName) {
         return Item.createItem(itemName, itemPrice, itemImageUrl, brandName, category, optionName);
     }
+
+    private static Item createItem1() {
+        return Item.createItem("NEW 루쥬 알뤼르 벨벳 뉘 블랑쉬 리미티드 에디션", 61000,
+                "https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20240319133310_1fda0cf74e4f43608184bce3050ae22a.jpg",
+                "샤넬", "뷰티", "00:00");
+    }
+
+    private static Item createItem2() {
+        return Item.createItem("NEW 루쥬 코코 밤(+샤넬 기프트 카드)", 51000,
+                "https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20220111185052_b92447cb764d470ead70b2d0fe75fe5c.jpg",
+                "샤넬", "뷰티", "934 코랄린 [NEW]");
+    }
+
+
 }
