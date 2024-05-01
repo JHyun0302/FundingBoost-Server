41526d602e976192b4d862ef6d85769a00f9817b
JHyun0302 <wogustkfkd11@gmail.com>
2024-05-01 16:09:39 +0900

    :white_check_mark: [Test] GiftHubController 테스트 코드 작성

---
+++ src/test/java/kcs/funding/fundingboost/domain/controller/GiftHubControllerTest.java
@@ -0,0 +1,149 @@
+package kcs.funding.fundingboost.domain.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+import kcs.funding.fundingboost.domain.dto.common.CommonSuccessDto;
+import kcs.funding.fundingboost.domain.dto.request.giftHub.AddGiftHubDto;
+import kcs.funding.fundingboost.domain.dto.request.giftHub.ItemQuantityDto;
+import kcs.funding.fundingboost.domain.dto.response.giftHub.GiftHubDto;
+import kcs.funding.fundingboost.domain.entity.GiftHubItem;
+import kcs.funding.fundingboost.domain.entity.Item;
+import kcs.funding.fundingboost.domain.entity.Member;
+import kcs.funding.fundingboost.domain.service.GiftHubItemService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(GiftHubController.class)
+class GiftHubControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GiftHubItemService giftHubItemService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Member member;
+    private Item item;
+    private GiftHubItem giftHubItem;
+
+    @BeforeEach
+    void setUp() throws NoSuchFieldException, IllegalAccessException {
+        member = createMember();
+        Field memberId = member.getClass().getDeclaredField("memberId");
+        memberId.setAccessible(true);
+        memberId.set(member, 1L);
+
+        item = createItem();
+        Field itemId = item.getClass().getDeclaredField("itemId");
+        itemId.setAccessible(true);
+        itemId.set(item, 1L);
+
+        giftHubItem = GiftHubItem.createGiftHubItem(1, item, member);
+        Field giftHunItemId = giftHubItem.getClass().getDeclaredField("giftHunItemId");
+        giftHunItemId.setAccessible(true);
+        giftHunItemId.set(giftHubItem, 1L);
+    }
+
+    @DisplayName("Gifthub 페이지 조회")
+    @Test
+    void giftHubPage() throws Exception {
+        List<GiftHubDto> giftHubDtoList = Collections.singletonList(
+                new GiftHubDto(item.getItemId(), item.getItemName(), item.getItemImageUrl(),
+                        item.getOptionName(), item.getItemPrice(), 1)
+        );
+
+        given(giftHubItemService.getGiftHub(member.getMemberId())).willReturn(giftHubDtoList);
+
+        mockMvc.perform(get("/api/v1/gifthub")
+                        .param("memberId", member.getMemberId().toString())
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data[0].itemName").value(item.getItemName()))
+                .andExpect(jsonPath("$.data[0].itemImageUrl").value(item.getItemImageUrl()))
+                .andExpect(jsonPath("$.data[0].optionName").value(item.getOptionName()))
+                .andExpect(jsonPath("$.data[0].itemPrice").value(item.getItemPrice()))
+                .andExpect(jsonPath("$.data[0].quantity").value(1));
+    }
+
+    @DisplayName("Gifthub에 담기")
+    @Test
+    void addGiftHub() throws Exception {
+        AddGiftHubDto addGiftHubDto = new AddGiftHubDto(member.getMemberId(), 1);
+        CommonSuccessDto expectedResponse = new CommonSuccessDto(true);
+
+        given(giftHubItemService.addGiftHub(any(Long.class), any(AddGiftHubDto.class)))
+                .willReturn(expectedResponse);
+
+        String content = objectMapper.writeValueAsString(addGiftHubDto);
+
+        mockMvc.perform(post("/api/v1/gifthub/{itemId}", item.getItemId())
+                        .contentType(APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.isSuccess").value(true));
+    }
+
+    @DisplayName("상품 수량 변경")
+    @Test
+    void patchGiftHubItem() throws Exception {
+        ItemQuantityDto itemQuantity = new ItemQuantityDto(5);
+        CommonSuccessDto expectedResponse = new CommonSuccessDto(true);
+
+        given(giftHubItemService.updateItem(any(Long.class), any(ItemQuantityDto.class)))
+                .willReturn(expectedResponse);
+
+        mockMvc.perform(patch("/api/v1/gifthub/quantity/{gifthubItemId}", giftHubItem.getGiftHunItemId())
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(itemQuantity)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.isSuccess").value(true));
+    }
+
+    @DisplayName("GiftHub 상품 삭제")
+    @Test
+    void deleteGiftHubItem() throws Exception {
+        CommonSuccessDto expectedResponse = new CommonSuccessDto(true);
+
+        given(giftHubItemService.deleteGiftHubItem(member.getMemberId(), giftHubItem.getGiftHunItemId()))
+                .willReturn(expectedResponse);
+
+        mockMvc.perform(delete("/api/v1/gifthub/{giftHubItemId}", giftHubItem.getGiftHunItemId())
+                        .param("memberId", member.getMemberId().toString())
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.isSuccess").value(true));
+    }
+
+    private static Member createMember() {
+        return Member.createMemberWithPoint("임창희", "dlackdgml3710@gmail.com", "",
+                "https://p.kakaocdn.net/th/talkp/wnbbRhlyRW/XaGAXxS1OkUtXnomt6S4IK/ky0f9a_110x110_c.jpg",
+                46000,
+                "", "aFxoWGFUZlV5SH9MfE9-TH1PY1JiV2JRaF83");
+    }
+
+    private static Item createItem() {
+        return Item.createItem("NEW 루쥬 알뤼르 벨벳 뉘 블랑쉬 리미티드 에디션", 61000,
+                "https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20240319133310_1fda0cf74e4f43608184bce3050ae22a.jpg",
+                "샤넬", "뷰티", "00:00");
+    }
+}
