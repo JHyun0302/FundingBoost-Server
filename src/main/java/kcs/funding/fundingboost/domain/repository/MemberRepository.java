650dc1d4101135f6977800111647c4b189dfaf96
koosco <rnxogud123@naver.com>
2024-05-01 12:09:25 +0900

    feat: nickName을 이용한 member 조회 메서드 추가

--- src/main/java/kcs/funding/fundingboost/domain/repository/MemberRepository.java
+++ src/main/java/kcs/funding/fundingboost/domain/repository/MemberRepository.java
@@ -1,7 +1,9 @@
 package kcs.funding.fundingboost.domain.repository;
 
+import java.util.Optional;
 import kcs.funding.fundingboost.domain.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByNickName(String nickName);
 }
