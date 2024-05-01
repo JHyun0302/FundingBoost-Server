3dd45c3d0a7323ce9f8dcf8a96289eb0b0e9dea5
koosco <rnxogud123@naver.com>
2024-05-01 12:09:10 +0900

    refactor: 공통적으로 사용되는 상수 저장 인터페이스 추가

---
+++ src/main/java/kcs/funding/fundingboost/domain/security/utils/SecurityConst.java
@@ -0,0 +1,7 @@
+package kcs.funding.fundingboost.domain.security.utils;
+
+public interface SecurityConst {
+    String AUTHORITIES_KEY = "auth";
+    String AUTHORIZATION_HEADER = "Authorization";
+
+}
