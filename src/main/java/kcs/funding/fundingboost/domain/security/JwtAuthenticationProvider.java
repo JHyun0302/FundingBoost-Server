e611d6be7c5b5b02c430e6ba27f90229b4523207
koosco <rnxogud123@naver.com>
2024-05-01 12:08:32 +0900

    feat: JwtAuthenticationProvider 추가
    * AuthenticationManager없이 Provider만 제공

---
+++ src/main/java/kcs/funding/fundingboost/domain/security/JwtAuthenticationProvider.java
@@ -0,0 +1,102 @@
+package kcs.funding.fundingboost.domain.security;
+
+import static kcs.funding.fundingboost.domain.security.utils.SecurityConst.AUTHORITIES_KEY;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtAuthenticationProvider implements InitializingBean {
+
+    private final String secret;
+    private final long tokenValidityInMilliseconds;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    private Key key;
+
+    public JwtAuthenticationProvider(@Value("${jwt.token}") String secret,
+                                     @Value("{jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds,
+                                     CustomUserDetailsService customUserDetailsService) {
+        this.secret = secret;
+        this.tokenValidityInMilliseconds = tokenValidityInMilliseconds;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createToken(Long memberId, Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(Long.toString(memberId)) // token 제목
+                .claim(AUTHORITIES_KEY, authorities) // member authority
+                .signWith(key, SignatureAlgorithm.HS512) // 암호화 알고리즘
+                .setExpiration(validity) // 유효 기간 설정
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException exception) {
+            log.info("잘못된 JWT 서명");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못됨");
+        }
+        return false;
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        List<SimpleGrantedAuthority> authorities = Arrays.stream(
+                        claims.get(AUTHORITIES_KEY).toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        UserDetails principal = customUserDetailsService.loadUserByUsername(claims.getSubject());
+        return new UsernamePasswordAuthenticationToken(principal, null, authorities);
+    }
+}
