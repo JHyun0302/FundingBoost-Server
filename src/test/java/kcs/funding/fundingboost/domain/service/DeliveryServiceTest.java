ee9935c36044d3a8cda6c9b67db12165d253f080
koosco <rnxogud123@naver.com>
2024-05-02 19:21:44 +0900

    refactor: 테스트 데이터 추가에 따른 코드 수정

--- src/test/java/kcs/funding/fundingboost/domain/service/DeliveryServiceTest.java
+++ src/test/java/kcs/funding/fundingboost/domain/service/DeliveryServiceTest.java
@@ -6,69 +6,66 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
-import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import kcs.funding.fundingboost.domain.dto.response.myPage.deliveryManage.MyPageDeliveryDto;
 import kcs.funding.fundingboost.domain.dto.response.myPage.deliveryManage.MyPageDeliveryManageDto;
 import kcs.funding.fundingboost.domain.entity.Delivery;
 import kcs.funding.fundingboost.domain.entity.Member;
 import kcs.funding.fundingboost.domain.exception.CommonException;
+import kcs.funding.fundingboost.domain.model.DeliveryFixture;
+import kcs.funding.fundingboost.domain.model.MemberFixture;
 import kcs.funding.fundingboost.domain.repository.DeliveryRepository;
 import kcs.funding.fundingboost.domain.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class DeliveryServiceTest {
     @Mock
     private DeliveryRepository deliveryRepository;
-
     @Mock
     private MemberRepository memberRepository;
-
     @InjectMocks
     private DeliveryService deliveryService;
 
     private Member member;
+    private Delivery delivery;
 
     @BeforeEach
     void setUp() throws NoSuchFieldException, IllegalAccessException {
-        member = createMember();
-        Field memberId = member.getClass().getDeclaredField("memberId");
-        memberId.setAccessible(true);
-        memberId.set(member, 1L);
+        member = MemberFixture.member1();
+        delivery = DeliveryFixture.address1(member);
     }
 
-    @DisplayName("마이페이지 - 배송지 조회 성공")
-    @ParameterizedTest(name = "{index} {displayName} arguments = {arguments}")
-    @CsvSource({
-            "'서울특별시 강남구 테헤란로', '010-1234-1234', '홍길동'",
-            "'경기도 수원시 행궁동', '010-1234-1234', '마동석'"
-    })
-    void getMyDeliveryManageList(String address, String phoneNumber, String customerName) {
+    @DisplayName("마이페이지 - getMyDeliveryManageList : 배송지 조회를 성공 deliveryList를 가져와서 Dto에 넣어주어야 한다")
+    @Test
+    void getMyDeliveryManageList() {
         //given
-        Delivery delivery = createDelivery(address, phoneNumber, customerName, member);
-        List<Delivery> deliveryList = Collections.singletonList(delivery);
+        List<Delivery> deliveryList = DeliveryFixture.addresses3(member);
 
         when(deliveryRepository.findAllByMemberId(anyLong())).thenReturn(deliveryList);
         when(memberRepository.findById(anyLong())).thenReturn(Optional.of(member));
 
         //when
         MyPageDeliveryManageDto myPageDeliveryManageDto = deliveryService.getMyDeliveryManageList(member.getMemberId());
+        List<MyPageDeliveryDto> myPageDeliveryDtos = myPageDeliveryManageDto.myPageDeliveryDtoList();
 
         //then
-        assertEquals(deliveryList.get(0).getAddress(),
-                myPageDeliveryManageDto.myPageDeliveryDtoList().get(0).address());
+        for (int i = 0; i < deliveryList.size(); i++) {
+            assertEquals(deliveryList.get(i).getCustomerName(), myPageDeliveryDtos.get(i).customerName());
+            assertEquals(deliveryList.get(i).getAddress(), myPageDeliveryDtos.get(i).address());
+            assertEquals(deliveryList.get(i).getPhoneNumber(), myPageDeliveryDtos.get(i).phoneNumber());
+        }
     }
 
-    @DisplayName("마이페이지 - 배송지 조회 성공 - 배송지 없음")
+    @DisplayName("마이페이지 - 배송지 조회 성공 - getMyDeliveryManageList : 배송지 목록이 없으면 빈 리스트를 반환해야 한다")
     @Test
     void getMyDeliveryManageList_DeliveryNotFound() {
         //given
@@ -79,36 +76,19 @@ class DeliveryServiceTest {
         assertEquals(List.of(), myPageDeliveryManageDto.myPageDeliveryDtoList());
     }
 
-    @DisplayName("마이페이지 - 배송지 조회 실패 - 사용자를 찾을 수 없음")
-    @ParameterizedTest(name = "{index} {displayName} arguments = {arguments}")
-    @CsvSource({
-            "'서울특별시 강남구 테헤란로', '010-1234-1234', '홍길동'",
-            "'경기도 수원시 행궁동', '010-1234-1234', '마동석'"
-    })
-    void getMyDeliveryManageList_MemberNotFound(String address, String phoneNumber, String customerName) {
+    @DisplayName("마이페이지 - 배송지 조회 실패 - getMyDeliveryManageList : 사용자가 없으면 NOT_FOUND_MEMBER 예외가 발생해야 한다")
+    @Test
+    void getMyDeliveryManageList_MemberNotFound() {
         //given
-        Delivery delivery = createDelivery(address, phoneNumber, customerName, member);
         List<Delivery> deliveryList = Collections.singletonList(delivery);
 
         when(deliveryRepository.findAllByMemberId(anyLong())).thenReturn(deliveryList);
-        when(memberRepository.findById(member.getMemberId())).thenReturn(Optional.ofNullable(member));
+        when(memberRepository.findById(anyLong())).thenReturn(Optional.empty());
 
-        //when
+        //when & then
         CommonException exception = assertThrows(CommonException.class, () -> {
             deliveryService.getMyDeliveryManageList(anyLong());
         }); // 사용자를 찾지 못했으므로 실패해야함
-        //then
         assertEquals(NOT_FOUND_MEMBER.getMessage(), exception.getMessage());
-    }
-
-
-    private static Member createMember() {
-        return Member.createMember("임창희", "dlackdgml3710@gmail.com", "",
-                "https://p.kakaocdn.net/th/talkp/wnbbRhlyRW/XaGAXxS1OkUtXnomt6S4IK/ky0f9a_110x110_c.jpg",
-                "", "aFxoWGFUZlV5SH9MfE9-TH1PY1JiV2JRaF83");
-    }
-
-    private static Delivery createDelivery(String address, String phoneNumber, String customerName, Member member) {
-        return Delivery.createDelivery(address, phoneNumber, customerName, member);
     }
 }
