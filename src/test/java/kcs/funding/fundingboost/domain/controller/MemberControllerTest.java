cdcd97492c497d1e86194ce8a07949a8c1086432
koosco <rnxogud123@naver.com>
2024-05-03 09:53:03 +0900

    refactor: 테스트 데이터 추가 클래스 통합

--- src/test/java/kcs/funding/fundingboost/domain/controller/MemberControllerTest.java
+++ src/test/java/kcs/funding/fundingboost/domain/controller/MemberControllerTest.java
@@ -7,16 +7,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.lang.reflect.Field;
-import java.time.LocalDateTime;
-import java.util.List;
 import kcs.funding.fundingboost.domain.dto.common.CommonSuccessDto;
 import kcs.funding.fundingboost.domain.dto.request.myPage.myFundingStatus.TransformPointDto;
 import kcs.funding.fundingboost.domain.entity.Funding;
-import kcs.funding.fundingboost.domain.entity.FundingItem;
 import kcs.funding.fundingboost.domain.entity.Item;
 import kcs.funding.fundingboost.domain.entity.Member;
-import kcs.funding.fundingboost.domain.entity.Tag;
+import kcs.funding.fundingboost.domain.model.FundingFixture;
+import kcs.funding.fundingboost.domain.model.FundingItemFixture;
+import kcs.funding.fundingboost.domain.model.ItemFixture;
+import kcs.funding.fundingboost.domain.model.MemberFixture;
 import kcs.funding.fundingboost.domain.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -43,32 +42,14 @@ class MemberControllerTest {
 
     private Member member;
     private Funding funding;
-    private Item item1;
+    private Item item;
 
     @BeforeEach
     void setUp() throws NoSuchFieldException, IllegalAccessException {
-        member = createMember();
-        Field memberId = member.getClass().getDeclaredField("memberId");
-        memberId.setAccessible(true);
-        memberId.set(member, 1L);
-        funding = createFunding();
-        Field fundingId = funding.getClass().getDeclaredField("fundingId");
-        fundingId.setAccessible(true);
-        fundingId.set(funding, 1L);
-
-        item1 = createItemId(
-                1L,
-                "NEW 루쥬 알뤼르 벨벳 뉘 블랑쉬 리미티드 에디션",
-                61000,
-                "https://img1.kakaocdn.net/...",
-                "샤넬",
-                "뷰티",
-                "00:00");
-
-        FundingItem fundingItem = FundingItem.createFundingItem(funding, item1, 1);
-        Field fundingItems = funding.getClass().getDeclaredField("fundingItems");
-        fundingItems.setAccessible(true);
-        fundingItems.set(funding, List.of(fundingItem));
+        member = MemberFixture.member1();
+        funding = FundingFixture.Graduate(member);
+        item = ItemFixture.item1();
+        FundingItemFixture.fundingItem1(item, funding);
     }
 
     @DisplayName("포인트 전환")
@@ -87,35 +68,5 @@ class MemberControllerTest {
                         .content(content))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.isSuccess").value(true));
-    }
-
-    private Funding createFunding() {
-        Funding funding = Funding.createFundingForTest(
-                member, "생일 선물 주세용", Tag.BIRTHDAY, 112000,
-                10000, LocalDateTime.of(2024, 4, 30, 23, 59));
-        return funding;
-    }
-
-    private static Member createMember() {
-        return Member.createMemberWithPoint("임창희", "dlackdgml3710@gmail.com", "",
-                "https://p.kakaocdn.net/th/talkp/wnbbRhlyRW/XaGAXxS1OkUtXnomt6S4IK/ky0f9a_110x110_c.jpg",
-                46000,
-                "", "aFxoWGFUZlV5SH9MfE9-TH1PY1JiV2JRaF83");
-    }
-
-    private static Item createItemId(
-            Long itemId1,
-            String itemName,
-            int itemPrice,
-            String itemImageUrl,
-            String brandName,
-            String category,
-            String optionName
-    ) throws NoSuchFieldException, IllegalAccessException {
-        Item item = Item.createItem(itemName, itemPrice, itemImageUrl, brandName, category, optionName);
-        Field itemId = item.getClass().getDeclaredField("itemId");
-        itemId.setAccessible(true);
-        itemId.set(item, itemId1);
-        return item;
     }
 }
