7795378e9c2e2dcbbf27d76fcfbdddc53d6aa295
koosco <rnxogud123@naver.com>
2024-05-08 01:43:02 +0900

    fix: 검증 로직 수정
    * jwt의 payload와 header를 이용
    * signature를 다시 계산하는 방식을 사용
    * BASE64 디코딩 방식 사용

--- src/main/java/kcs/funding/fundingboost/domain/security/JwtAuthenticationProvider.java
+++ src/main/java/kcs/funding/fundingboost/domain/security/JwtAuthenticationProvider.java
@@ -1,77 +1,47 @@
 package kcs.funding.fundingboost.domain.security;
 
-import static kcs.funding.fundingboost.domain.security.utils.SecurityConst.AUTHORITIES_KEY;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.UnsupportedJwtException;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import java.security.Key;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.Base64;
 import java.util.List;
-import java.util.stream.Collectors;
+import kcs.funding.fundingboost.domain.security.utils.JwtUtils;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class JwtAuthenticationProvider implements InitializingBean {
+@RequiredArgsConstructor
+public class JwtAuthenticationProvider {
 
-    private final String secret;
-    private final long tokenValidityInMilliseconds;
     private final CustomUserDetailsService customUserDetailsService;
-
-    private Key key;
-
-    public JwtAuthenticationProvider(@Value("${jwt.secret}") String secret,
-                                     @Value("${jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds,
-                                     CustomUserDetailsService customUserDetailsService) {
-        this.secret = secret;
-        this.tokenValidityInMilliseconds = tokenValidityInMilliseconds;
-        this.customUserDetailsService = customUserDetailsService;
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
-        this.key = Keys.hmacShaKeyFor(keyBytes);
-    }
-
-    public String createToken(Authentication authentication) {
-        String authorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
-
-        long now = (new Date()).getTime();
-        Date validity = new Date(now + this.tokenValidityInMilliseconds);
-
-        return Jwts.builder()
-                .setSubject(authentication.getName()) // token 제목
-                .claim(AUTHORITIES_KEY, authorities) // member authority
-                .signWith(key, SignatureAlgorithm.HS512) // 암호화 알고리즘
-                .setExpiration(validity) // 유효 기간 설정
-                .compact();
-    }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
+
+            String[] parts = token.split("\\.");
+            String payLoad = parts[1];
+            String signature = parts[2];
+
+            String decodedPayload = new String(Base64.getDecoder().decode(payLoad));
+
+            String calculatedJwt = Jwts.builder()
+                    .signWith(JwtUtils.getKey(), SignatureAlgorithm.HS512)
+                    .setHeaderParam("typ", "JWT")
+                    .setPayload(decodedPayload)
+                    .compact();
+
+            String calculatedSignature = calculatedJwt.split("\\.")[2];
+
+            return signature.equals(calculatedSignature);
         } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException exception) {
             log.info("잘못된 JWT 서명");
         } catch (ExpiredJwtException e) {
@@ -86,17 +56,13 @@ public class JwtAuthenticationProvider implements InitializingBean {
 
     public Authentication getAuthentication(String token) {
         Claims claims = Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(JwtUtils.getKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
 
-        List<SimpleGrantedAuthority> authorities = Arrays.stream(
-                        claims.get(AUTHORITIES_KEY).toString().split(","))
-                .map(SimpleGrantedAuthority::new)
-                .toList();
-
         UserDetails principal = customUserDetailsService.loadUserByUsername(claims.getSubject());
-        return new UsernamePasswordAuthenticationToken(principal, null, authorities);
+        return new UsernamePasswordAuthenticationToken(principal, null,
+                List.of(new SimpleGrantedAuthority("ROLE_USER")));
     }
 }
