505263fe81ba8c8176f6de952b8902f4d1168987
koosco <rnxogud123@naver.com>
2024-05-08 01:56:57 +0900

    feat: userDetailsService에서 id를 이용한 조회 로직 추가

--- src/main/java/kcs/funding/fundingboost/domain/security/CustomUserDetailsService.java
+++ src/main/java/kcs/funding/fundingboost/domain/security/CustomUserDetailsService.java
@@ -1,8 +1,9 @@
 package kcs.funding.fundingboost.domain.security;
 
+import static kcs.funding.fundingboost.domain.exception.ErrorCode.NOT_FOUND_MEMBER;
+
 import kcs.funding.fundingboost.domain.entity.member.Member;
 import kcs.funding.fundingboost.domain.exception.CommonException;
-import kcs.funding.fundingboost.domain.exception.ErrorCode;
 import kcs.funding.fundingboost.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,7 +20,15 @@ public class CustomUserDetailsService implements UserDetailsService {
     public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // 회원 이름이 있는지 먼저 검증
         Member member = memberRepository.findByNickName(username).orElseThrow(
-                () -> new CommonException(ErrorCode.NOT_FOUND_MEMBER)
+                () -> new CommonException(NOT_FOUND_MEMBER)
+        );
+
+        return new CustomUserDetails(member);
+    }
+
+    public CustomUserDetails loadUserByUserId(Long userId) {
+        Member member = memberRepository.findById(userId).orElseThrow(
+                () -> new CommonException(NOT_FOUND_MEMBER)
         );
 
         return new CustomUserDetails(member);
