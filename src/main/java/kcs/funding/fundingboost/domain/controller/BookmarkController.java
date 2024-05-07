c5697fb0ac8de8f9731cb1dd368a053f5373dd68
koosco <rnxogud123@naver.com>
2024-05-08 02:09:54 +0900

    refactor: Jwt를 이용해 userId 획득하도록 변경

--- src/main/java/kcs/funding/fundingboost/domain/controller/BookmarkController.java
+++ src/main/java/kcs/funding/fundingboost/domain/controller/BookmarkController.java
@@ -3,6 +3,7 @@ package kcs.funding.fundingboost.domain.controller;
 import kcs.funding.fundingboost.domain.dto.common.CommonSuccessDto;
 import kcs.funding.fundingboost.domain.dto.global.ResponseDto;
 import kcs.funding.fundingboost.domain.dto.response.myPage.wishList.MyBookmarkListDto;
+import kcs.funding.fundingboost.domain.security.resolver.Login;
 import kcs.funding.fundingboost.domain.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -22,7 +22,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping("")
-    public ResponseDto<MyBookmarkListDto> viewMyFavoriteListDto(@RequestParam("memberId") Long memberId) {
+    public ResponseDto<MyBookmarkListDto> viewMyFavoriteListDto(@Login Long memberId) {
         return ResponseDto.ok(bookmarkService.getMyBookmark(memberId));
     }
 
@@ -30,7 +30,7 @@ public class BookmarkController {
      * 상품 좋아요
      */
     @PostMapping("/like/{itemId}")
-    public ResponseDto<CommonSuccessDto> itemLike(@RequestParam(name = "memberId") Long memberId,
+    public ResponseDto<CommonSuccessDto> itemLike(@Login Long memberId,
                                                   @PathVariable(name = "itemId") Long itemId) {
         return ResponseDto.ok(bookmarkService.toggleItemLike(memberId, itemId));
     }
