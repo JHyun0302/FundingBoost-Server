40ffee2de036fa4db9019f4bb45e8886024a497d
koosco <rnxogud123@naver.com>
2024-05-08 01:41:55 +0900

    refactor: Dto에서 token 앞에 PREFIX를 추가하도록 수정

--- src/main/java/kcs/funding/fundingboost/domain/dto/response/login/TokenDto.java
+++ src/main/java/kcs/funding/fundingboost/domain/dto/response/login/TokenDto.java
@@ -1,8 +1,10 @@
 package kcs.funding.fundingboost.domain.dto.response.login;
 
+import static kcs.funding.fundingboost.domain.security.utils.SecurityConst.TOKEN_PREFIX;
+
 public record TokenDto(String token) {
 
     public static TokenDto fromEntity(String token) {
-        return new TokenDto(token);
+        return new TokenDto(TOKEN_PREFIX + token);
     }
 }
