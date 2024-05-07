597a2d81b5f25e877163abf0e2fcda5c27489f3f
koosco <rnxogud123@naver.com>
2024-05-08 01:43:34 +0900

    refactor: token 생성 로직 수정

--- src/main/java/kcs/funding/fundingboost/domain/service/AuthService.java
+++ src/main/java/kcs/funding/fundingboost/domain/service/AuthService.java
@@ -1,39 +1,36 @@
 package kcs.funding.fundingboost.domain.service;
 
-import static kcs.funding.fundingboost.domain.security.utils.SecurityConst.AUTHORIZATION_HEADER;
 import static kcs.funding.fundingboost.domain.security.utils.SecurityConst.TOKEN_PREFIX;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 import kcs.funding.fundingboost.domain.dto.request.login.LoginDto;
 import kcs.funding.fundingboost.domain.dto.response.login.TokenDto;
-import kcs.funding.fundingboost.domain.security.JwtAuthenticationProvider;
+import kcs.funding.fundingboost.domain.security.CustomUserDetails;
+import kcs.funding.fundingboost.domain.security.SimpleAuthenticationProvider;
+import kcs.funding.fundingboost.domain.security.utils.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final JwtAuthenticationProvider jwtAuthenticationProvider;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final SimpleAuthenticationProvider authenticationProvider;
 
+    /**
+     * 토큰이 없는 사용자는 SimpleAuthenticationProvider가 검증을 하고 검증에 성공하면 Jwt Token을 생성해서 반환
+     */
     public ResponseEntity<TokenDto> createJwtToken(LoginDto loginDto) {
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginDto.nickName(),
-                loginDto.password());
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(token);
-        SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(authentication);
-
-        String jwt = jwtAuthenticationProvider.createToken(authentication);
+        CustomUserDetails userDetails = authenticationProvider.validate(loginDto.nickName(), loginDto.password());
+        // username과 password를 이용해 token 생성
+        String jwt = JwtUtils.createToken(userDetails);
 
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(AUTHORIZATION_HEADER, TOKEN_PREFIX + jwt);
+        httpHeaders.add(AUTHORIZATION, TOKEN_PREFIX + jwt); // header에 토큰을 추가
 
-        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+        return new ResponseEntity<>(TokenDto.fromEntity(jwt), httpHeaders, HttpStatus.OK);
     }
 }
