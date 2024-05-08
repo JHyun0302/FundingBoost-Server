782a7d7aed5e584aadd47224d642cfb8a4137e03
MeangSung <aoddlsgh98@gmail.com>
2024-05-08 14:04:40 +0900

    refactor: 시간 차이를 이용하여 펀딩종료 표현을 하기 위해 시간차를 LocalDate -> LocalDateTime으로 변경

--- src/main/java/kcs/funding/fundingboost/domain/service/utils/DateUtils.java
+++ src/main/java/kcs/funding/fundingboost/domain/service/utils/DateUtils.java
@@ -2,13 +2,13 @@ package kcs.funding.fundingboost.domain.service.utils;
 
 import static kcs.funding.fundingboost.domain.service.utils.FundingConst.FUNDING_FINISHED_MESSAGE;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import kcs.funding.fundingboost.domain.entity.Funding;
 
 public class DateUtils {
     public static String toDeadlineString(Funding funding) {
-        int leftDate = (int) ChronoUnit.DAYS.between(LocalDate.now(), funding.getDeadline());
+        int leftDate = (int) ChronoUnit.SECONDS.between(LocalDateTime.now(), funding.getDeadline());
         if (leftDate >= 0) {
             return "D-" + leftDate;
         } else {
