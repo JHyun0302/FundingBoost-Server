3e865df617e0f8d040053878943cf60786e304d8
koosco <rnxogud123@naver.com>
2024-05-08 09:07:02 +0900

    refactor: PasswordEncoder 관련 config 추가

--- src/main/java/kcs/funding/fundingboost/domain/config/SecurityConfig.java
+++ src/main/java/kcs/funding/fundingboost/domain/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -50,10 +49,5 @@ public class SecurityConfig {
                 .with(new JwtSecurityConfig(jwtAuthenticationProvider), customizer -> {
                 });
         return http.build();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
