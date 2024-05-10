2cb6dc5006e779a94d9eaf1e07c2e9e81625cc15
ChangHee98 <70644043+ChangHee98@users.noreply.github.com>
2024-05-10 16:07:39 +0900

    refactor: 친구 펀딩 상세 페이지 leftPrice, deadlineDate FriendFundingDetailDto에 추가

--- src/main/java/kcs/funding/fundingboost/domain/dto/response/friendFundingDetail/FriendFundingDetailDto.java
+++ src/main/java/kcs/funding/fundingboost/domain/dto/response/friendFundingDetail/FriendFundingDetailDto.java
@@ -4,6 +4,7 @@ package kcs.funding.fundingboost.domain.dto.response.friendFundingDetail;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import kcs.funding.fundingboost.domain.entity.Funding;
+import kcs.funding.fundingboost.domain.service.utils.DateUtils;
 import lombok.Builder;
 
 @Builder
@@ -11,13 +12,13 @@ public record FriendFundingDetailDto(List<FriendFundingItemDto> friendFundingIte
                                      List<ContributorDto> contributorList,
                                      String friendName, String friendProfileImgUrl,
                                      String deadline, int contributedPercent,
-                                     String fundingTag, String fundingMessage) {
+                                     String fundingTag, String fundingMessage,
+                                     String deadLineDate, int leftPrice) {
 
 
     public static FriendFundingDetailDto fromEntity(List<FriendFundingItemDto> friendFundingItemList, Funding funding,
                                                     List<ContributorDto> contributorList, int contributedPercent
     ) {
-
         return FriendFundingDetailDto.builder()
                 .friendFundingItemList(friendFundingItemList)
                 .friendName(funding.getMember().getNickName())
@@ -27,6 +28,8 @@ public record FriendFundingDetailDto(List<FriendFundingItemDto> friendFundingIte
                 .contributorList(contributorList)
                 .deadline(funding.getDeadline().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
                 .contributedPercent(contributedPercent)
+                .deadLineDate(DateUtils.toDeadlineString(funding))
+                .leftPrice(funding.getTotalPrice() - funding.getCollectPrice())
                 .build();
     }
 }
