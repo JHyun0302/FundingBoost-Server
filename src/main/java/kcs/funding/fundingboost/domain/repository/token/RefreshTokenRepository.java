6e67da1a118c0fc085bc993106f744164429d7e4
koosco <rnxogud123@naver.com>
2024-05-13 17:21:10 +0900

    chore: RefreshTokenRepository 패키지 구조 변경
    * security package 안으로 위치를 변경

--- src/main/java/kcs/funding/fundingboost/domain/repository/token/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package kcs.funding.fundingboost.domain.repository.token;
-
-import kcs.funding.fundingboost.domain.entity.token.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
-}
