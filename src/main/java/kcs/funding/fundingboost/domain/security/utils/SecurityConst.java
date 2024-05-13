05d5449c0c86cbe1163bdf7313d0ef4aeec6950c
koosco <rnxogud123@naver.com>
2024-05-13 14:46:35 +0900

    feat: accessToken, refreshToken validity time 추가

--- src/main/java/kcs/funding/fundingboost/domain/security/utils/SecurityConst.java
+++ src/main/java/kcs/funding/fundingboost/domain/security/utils/SecurityConst.java
@@ -3,6 +3,7 @@ package kcs.funding.fundingboost.domain.security.utils;
 public interface SecurityConst {
     String AUTHORITIES_KEY = "auth";
     String TOKEN_PREFIX = "Bearer ";
-    long tokenValidityInMilliseconds = 8640000;
+    long refreshTokenValidityInMilliseconds = 864000000;
+    long accessTokenValidityInMilliseconds = 864000;
     String secret = "tmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmptmp";
 }
