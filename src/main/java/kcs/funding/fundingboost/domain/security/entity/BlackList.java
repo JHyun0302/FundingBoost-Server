8fb491b470244120c5381aa26ddda1483a5e0f7f
koosco <rnxogud123@naver.com>
2024-05-23 13:50:24 +0900

    fix: access token validity time 수정

--- src/main/java/kcs/funding/fundingboost/domain/security/entity/BlackList.java
+++ src/main/java/kcs/funding/fundingboost/domain/security/entity/BlackList.java
@@ -1,11 +1,13 @@
 package kcs.funding.fundingboost.domain.security.entity;
 
+import static kcs.funding.fundingboost.domain.security.utils.SecurityConst.ACCESS_TOKEN_VALIDITY_IN_SECONDS;
+
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
-@RedisHash(value = "accessToken", timeToLive = 5)
+@RedisHash(value = "accessToken", timeToLive = ACCESS_TOKEN_VALIDITY_IN_SECONDS)
 public class BlackList {
 
     @Id
