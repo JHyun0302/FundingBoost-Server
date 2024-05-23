22c8cad0e5033d824e1dc9313d9a8a54da988b5e
MeangSung <aoddlsgh98@gmail.com>
2024-05-23 17:43:43 +0900

    refactor: OAuth2 token 생성 수정

--- src/main/java/kcs/funding/fundingboost/domain/security/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/kcs/funding/fundingboost/domain/security/handler/OAuth2AuthenticationSuccessHandler.java
@@ -23,7 +23,7 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
-        String accessToken = "Bearer " + jwtAuthenticationService.createAccessToken(authentication);
+        String accessToken = jwtAuthenticationService.createAccessToken(authentication);
         String refreshToken = jwtAuthenticationService.createRefreshToken(authentication).getToken();
 
         JwtDto jwtTokenDto = JwtDto.fromEntity(accessToken, refreshToken);
