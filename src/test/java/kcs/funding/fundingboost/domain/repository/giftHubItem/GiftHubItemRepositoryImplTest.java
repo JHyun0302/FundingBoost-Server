88d76b72642dbfe575ed196ed3ea7b1b56949607
ChangHee98 <70644043+ChangHee98@users.noreply.github.com>
2024-05-23 17:18:08 +0900

    feat: GiftHubItemRepository Test 코드 구현

---
+++ src/test/java/kcs/funding/fundingboost/domain/repository/giftHubItem/GiftHubItemRepositoryImplTest.java
@@ -0,0 +1,71 @@
+package kcs.funding.fundingboost.domain.repository.giftHubItem;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import kcs.funding.fundingboost.domain.config.QueryDslConfig;
+import kcs.funding.fundingboost.domain.entity.GiftHubItem;
+import kcs.funding.fundingboost.domain.entity.Item;
+import kcs.funding.fundingboost.domain.entity.member.Member;
+import kcs.funding.fundingboost.domain.model.GiftHubItemFixture;
+import kcs.funding.fundingboost.domain.model.ItemFixture;
+import kcs.funding.fundingboost.domain.model.MemberFixture;
+import kcs.funding.fundingboost.domain.repository.ItemRepository;
+import kcs.funding.fundingboost.domain.repository.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest
+@Import(QueryDslConfig.class)
+@Transactional
+class GiftHubItemRepositoryImplTest {
+
+    @Autowired
+    private GiftHubItemRepository giftHubItemRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    private Member member;
+    private Item item;
+    private GiftHubItem giftHubItem;
+
+    @BeforeEach
+    void setUp() throws NoSuchFieldException, IllegalAccessException {
+        member = MemberFixture.member1();
+        item = ItemFixture.item1();
+    }
+
+    @AfterEach
+    void after() {
+        giftHubItemRepository.deleteAll();
+        itemRepository.deleteAll();
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    void findGiftHubItemsByMember() throws NoSuchFieldException, IllegalAccessException {
+
+        Member savedMember = memberRepository.save(member);
+        Item savedItem = itemRepository.save(item);
+        giftHubItem = GiftHubItemFixture.quantity1(savedItem, savedMember);
+        GiftHubItem savedGiftHubItem = giftHubItemRepository.save(giftHubItem);
+
+        //given
+        List<GiftHubItem> result = giftHubItemRepository.findGiftHubItemsByMember(savedMember.getMemberId());
+
+        //then
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getGiftHubItemId()).isEqualTo(savedGiftHubItem.getGiftHubItemId());
+        assertThat(result.get(0).getMember().getNickName()).isEqualTo(savedMember.getNickName());
+        assertThat(result.get(0).getItem().getItemName()).isEqualTo(savedItem.getItemName());
+    }
+}
