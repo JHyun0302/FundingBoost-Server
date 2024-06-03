f2aae13b28f381e509132661427f622fdae960da
koosco <rnxogud123@naver.com>
2024-06-04 00:04:38 +0900

    fix: 구현하는 인터페이스 오류 수정

--- src/main/java/kcs/funding/fundingboost/elasticsearch/repository/ItemIndexRepository.java
+++ src/main/java/kcs/funding/fundingboost/elasticsearch/repository/ItemIndexRepository.java
@@ -1,8 +1,7 @@
 package kcs.funding.fundingboost.elasticsearch.repository;
 
-import kcs.funding.fundingboost.domain.repository.item.ItemRepositoryCustom;
 import kcs.funding.fundingboost.elasticsearch.index.ItemIndex;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface ItemIndexRepository extends ElasticsearchRepository<ItemIndex, Long>, ItemRepositoryCustom {
+public interface ItemIndexRepository extends ElasticsearchRepository<ItemIndex, Long>, ItemIndexRepositoryCustom {
 }
