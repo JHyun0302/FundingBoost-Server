c53fbaa362203c3c408919c0321cb5876f0ba50f
MeangSung <aoddlsgh98@gmail.com>
2024-06-03 14:11:37 +0900

    Refactor: kakao oauth 로그인 최종 수정

--- src/main/java/kcs/funding/fundingboost/domain/config/AuthenticationConfig.java
+++ src/main/java/kcs/funding/fundingboost/domain/config/AuthenticationConfig.java
@@ -1,7 +1,5 @@
 package kcs.funding.fundingboost.domain.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import kcs.funding.fundingboost.domain.security.service.JwtAuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,22 +9,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @RequiredArgsConstructor
 public class AuthenticationConfig {
-
-    private final JwtAuthenticationService jwtAuthenticationService;
-    private final ObjectMapper objectMapper;
+    
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Bean
-    public OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler() {
-        return new OAuth2AuthenticationSuccessHandler(jwtAuthenticationService, objectMapper);
-    }
-
-    @Bean
-    public OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler() {
-        return new OAuth2AuthenticationFailureHandler();
-    }
 }
