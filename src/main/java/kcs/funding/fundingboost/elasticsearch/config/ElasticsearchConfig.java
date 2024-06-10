aa3620edd2e461304cbc96150032da10cb22e41b
wogustkfkd11 <wogustkfkd11@naver.com>
2024-06-10 03:10:05 +0000

    update elastic

--- src/main/java/kcs/funding/fundingboost/elasticsearch/config/ElasticsearchConfig.java
+++ src/main/java/kcs/funding/fundingboost/elasticsearch/config/ElasticsearchConfig.java
@@ -1,40 +1,15 @@
 package kcs.funding.fundingboost.elasticsearch.config;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 
 @Configuration
-@EnableElasticsearchRepositories
-public class ElasticsearchConfig {
-
-    @Value("${spring.data.elasticsearch.host}")
-    private String host;
-
-    @Value("${spring.data.elasticsearch.port}")
-    private int port;
-
-    @Bean
-    public RestClient restClient() {
-        return RestClient
-                .builder(new HttpHost(host, port, "http"))
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo("elasticsearch:9200")
                 .build();
-    }
-
-    @Bean
-    public ElasticsearchTransport elasticsearchTransport(RestClient restClient) {
-        return new RestClientTransport(restClient, new JacksonJsonpMapper());
-    }
-
-    @Bean
-    public ElasticsearchClient elasticsearchClient(ElasticsearchTransport transport) {
-        return new ElasticsearchClient(transport);
     }
 }
